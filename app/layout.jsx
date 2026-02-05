68999d4c0441d5c7e48734abc0a489a2b74a42ea
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2026-02-05 16:34:45 +0100

    fix: update docsRepositoryBase URL to point to the correct repository

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,7 +37,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/lorenzo-vecchio/endpoint-fetcher-docs"
           footer={footer}
           // ... Your additional layout options
         >
