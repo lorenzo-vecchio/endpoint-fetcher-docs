e724509db30b7c525762b0abda9d2695c7eae87f
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2026-02-05 18:59:46 +0100

    refactor: reorganize next.config.mjs for clarity and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,25 @@
 import nextra from 'nextra'
 
-// Set up Nextra with its configuration
-const withNextra = nextra({
-    // ... Add Nextra-specific options here
-})
-
-// Export the final Next.js config with Nextra included
-export default withNextra({
-    // ... Add regular Next.js options here
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true // mandatory, otherwise won't export
+    },
     turbopack: {
         resolveAlias: {
             // Path to your `mdx-components` file with extension
             'next-mdx-import-source-file': './mdx-components.tsx'
         }
     }
+}
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+    // ... Add Nextra-specific options here
 })
+
+// Export the final Next.js config with Nextra included
+export default withNextra(nextConfig)
