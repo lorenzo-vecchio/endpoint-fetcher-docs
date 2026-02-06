a0d43606eedf7d32d852b4911ccb4497809aaa7b
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2026-02-06 10:14:56 +0100

    feat: add initial documentation for configuration options

---
+++ content/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+    index: "Getting Started",
+    basic_usage: "Basic Usage",
+    groups: "Groups & Organization",
+    hooks: "Hierarchical Hooks",
+    configuration: "Configuration",
+}
